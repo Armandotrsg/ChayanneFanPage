0010e839ded8de22119799c072a8e58420b07511
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-02-21 17:58:42 -0600

    Missing to upload comments

--- js/comments.js
+++ js/comments.js
@@ -5,7 +5,6 @@ function loadComments() {
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
       let comments = JSON.parse(xhr.responseText);
-      comments = comments["comments"];
       console.log(comments);
       comments.map((comment) => {
         let li = document.createElement("li");
@@ -28,6 +27,50 @@ function loadComments() {
   xhr.open("GET", url, true);
   xhr.send();
 }
+
+//TODO: Fix this function
+function addComment(commentData) {
+  let url = "./db/db.json";
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      loadComments();
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let comments = JSON.parse(xhr.responseText);
+      comments.push(commentData);
+      let putXhr = new XMLHttpRequest();
+      putXhr.onreadystatechange = function () {
+        if (putXhr.readyState == 4 && putXhr.status == 200) {
+          loadComments();
+        }
+      };
+      putXhr.open("PUT", url, true);
+      putXhr.setRequestHeader("Content-Type", "application/json");
+      putXhr.send(JSON.stringify(comments));
+    }
+  };
+}
+
+$("#commentForm").submit((e) => {
+  e.preventDefault();
+  let name = $("#name").val();
+  let comment = $("#comment").val();
+  let date = new Date().toLocaleDateString();
+  let commentData = {
+    name: name,
+    date: date,
+    comment: comment, 
+  };
+    addComment(commentData);
+    $("#name").val("");
+    $("#comment").val("");
+});
 
 $(document).ready(function () {
   loadComments();
